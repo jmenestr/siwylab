63b4d9febd57534239821cc6eb5b72c70a5d6217
Justin Menestrina <jmenestr@uci.edu>
2015-03-20 13:47:18 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ $(document).ready(function() {
     $("#copyright").text("Copyright " + year +" Swiy Lab")
 
     $("#research_list a").each(function() {
-        var id = $(this).data("topic");
-        $(this).click(function(e) {
+        var id = $(this).data("topic"); //Get data id tag to assign correct #id
+        $(this).click(function(e) { //Assign click handeler
             e.preventDefault();
-            if(!$("#"+id).hasClass("show")) {
+            if(!$("#"+id).hasClass("show")) { //If different topic clicked than one currently showed, hides current
                 $("#research section").each(function() {
                     if($(this).hasClass("show")) {
-                        $(this).toggleClass("show");
+                        $(this).toggleClass("show"); //Togglles show class
                     }
                 });
             }
