75f2aaf8a4c16e39872ec3c328589e7dab21eaa2
Justin Menestrina <jmenestr@uci.edu>
2015-03-20 13:57:02 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,19 +5,19 @@ $(document).ready(function() {
     var date = new Date();
     var year = date.getFullYear();
     $("#copyright").text("Copyright " + year +" Swiy Lab")
-
+    
+    function hide_other_topcis(selected_topic) {
+        if(!$(selected_topic).hasClass("show")){
+            $("#research section").each(function() {
+                $(this).removeClass("show");
+            });
+        }
+    }
     $("#research_list a").each(function() {
         var id = $(this).data("topic"); //Get data id tag to assign correct #id
         $(this).click(function(e) { //Assign click handeler
             e.preventDefault();
-            if(!$("#"+id).hasClass("show")) { //If different topic clicked than one currently showed, hides current
-                $("#research section").each(function() {
-                    if($(this).hasClass("show")) {
-                        $(this).toggleClass("show"); //Togglles show class
-                    }
-                });
-            }
-
+            hide_other_topics("#"+id);
             $("#"+id).toggleClass("show");
         });
 
