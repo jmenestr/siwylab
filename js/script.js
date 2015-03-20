99f49a0dfd34e89e955f00b9ee8637ae431893be
Justin Menestrina <jmenestr@uci.edu>
2015-03-20 11:18:10 -0700

    Research Page outlined and added old Reserach page in meantime

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@
  * Created by Justin M on 3/10/2015.
  */
 $(document).ready(function() {
-var date = new Date();
+    var date = new Date();
     var year = date.getFullYear();
     $("#copyright").text("Copyright " + year +" Swiy Lab")
+
+    $(".banner").unslider();
 
 });
